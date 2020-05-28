2f83e03994eecfaab05a71a50c629d397dda0e55
Cooper Pearson <mptprogesterone@gmail.com>
2020-05-28 11:15:07 -0500

    add to tail

--- linked-lists.js
+++ linked-lists.js
@@ -1,15 +1,15 @@
 function LinkedList() {
-this.head = null;
-this.tail = null;
+    this.head = null;
+    this.tail = null;
 }
 
 function Node(value, next, prev) {
     this.value = value;
-    this.next  = next;
-    this.prev  = prev;
+    this.next = next;
+    this.prev = prev;
 }
 
-LinkedList.prototype.addToHead = function(value) {
+LinkedList.prototype.addToHead = function (value) {
     var newNode = new Node(value, this.head, null);
     if (this.head) this.head.prev = newNode;
     else this.tail = newNode;
@@ -24,10 +24,20 @@ ll.addToHead(300);
 
 console.log(ll);
 
-LinkedList.prototype.addToTail = function(value){
+LinkedList.prototype.addToTail = function (value) {
     var newNode = new Node(value, null, this.tail);
-    if(this.tail) this.tail.next = newNode;
+    if (this.tail) this.tail.next = newNode;
     else this.head = newNode;
     this.tail = newNode;
 };
+var myLL = new LinkedList();
 
+myLL.addToTail(10);
+myLL.addToTail(20);
+myLL.addToTail(30);
+myLL.addToHead(100);
+
+console.log(myLL);
+console.log(myLL.tail.prev);
+console.log(myLL.tail.prev.prev);
+console.log(myLL.tail.prev.prev.prev);
