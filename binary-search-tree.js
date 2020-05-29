eab7afebfc07cd86936cbaa3c725632379e5c9d5
Cooper Pearson <mptprogesterone@gmail.com>
2020-05-29 11:20:20 -0500

    pre order

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -48,6 +48,7 @@ BST.prototype.contains = function (value) {
 //depth first traversel method with recursion
 
 BST.prototype.depthFirstTraversal = function (iteratorFunc, order) {
+    if (order === 'pre-order') iteratorFunc(this.value);
     if (this.left) this.left.depthFirstTraversal(iteratorFunc, order);
     if (order === 'in-order') iteratorFunc(this.value);
     if (this.right) this.right.depthFirstTraversal(iteratorFunc, order);
