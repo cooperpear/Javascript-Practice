494cd45b03fd4234dcbd0a56e322e48dee81ed1b
Cooper Pearson <mptprogesterone@gmail.com>
2020-05-29 10:16:04 -0500

    contains method

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -30,3 +30,18 @@ BST.prototype.insert = function (value){
         else this.right.insert(value);
     }
 };
+
+//Create and insert values
+var bst = new BST(50);
+bst.insert(30);
+bst.insert(70);
+bst.insert(20);
+bst.insert(10);
+bst.insert(45);
+bst.insert(35);
+bst.insert(70);
+bst.insert(60);
+bst.insert(100);
+bst.insert(85);
+bst.insert(100);
+bst.insert(59);
