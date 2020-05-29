1bfb1f3b4720190eb71979c3c812bdc185e89c6b
Cooper Pearson <mptprogesterone@gmail.com>
2020-05-29 09:39:29 -0500

    factorial example

---
+++ binary-search-tree.js
@@ -0,0 +1,11 @@
+//recursion with factorial
+function factorial(num) {
+    if (num === 1) {
+        return num 
+    }
+    else {
+        return num * factorial(num-1);
+    }
+};
+
+factorial(4);
