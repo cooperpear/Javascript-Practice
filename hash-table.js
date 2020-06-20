34a9a69b43bfab2803452c380865816d20510c0a
Cooper Pearson <mptprogesterone@gmail.com>
2020-06-20 14:55:50 -0500

    loop through keys

--- hash-table.js
+++ hash-table.js
@@ -9,6 +9,13 @@ function HashNode(key, value, next) {
     this.next = next || null;
 }
 
+HashTable.prototype.hash = function (key) {
+    var total = 0;
+    for (var i = 0; i < key.length; i++) {
+        total += key.charCodeAt(i);
+    }
+};
+
 var myHT = new HashTable(30);
 
 console.log(myHT);
